9019724aacad4ef19b200a7fd6365c51bab22b08
Hazzard13 <nathanhazz@gmail.com>
2015-11-30 22:46:33 -0500

    Made the main textview a scrolling activity in case of more tasks than the screen can display.

--- app/src/main/java/com/hazzard/nathan/to_do/MainActivity.java
+++ app/src/main/java/com/hazzard/nathan/to_do/MainActivity.java
@@ -3,6 +3,7 @@ package com.hazzard.nathan.to_do;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView displayText = (TextView) findViewById(R.id.body);
+        displayText.setMovementMethod(new ScrollingMovementMethod());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
