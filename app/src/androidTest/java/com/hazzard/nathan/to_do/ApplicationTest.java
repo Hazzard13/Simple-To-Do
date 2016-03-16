a2c699b5ba09adbecc842014bd2262db0b4666ee
Hazzard13 <nathanhazz@gmail.com>
2016-03-15 22:35:14 -0400

    Learned basics of Android Testing, simple android instrumentation test added to check that tasks are being saved and loaded correctly.

--- app/src/androidTest/java/com/hazzard/nathan/to_do/ApplicationTest.java
+++ app/src/androidTest/java/com/hazzard/nathan/to_do/ApplicationTest.java
@@ -3,11 +3,36 @@ package com.hazzard.nathan.to_do;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private Application main;
+    private ArrayList<Task> storedTaskList;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createApplication();
+        main = getApplication();
+        storedTaskList = TaskListManager.loadTaskList(main);
+    }
+
+    public void testSaveAndLoadTaskList() {
+        ArrayList <Task> taskList = new ArrayList<Task>();
+        taskList.add(new Task("Sample1", new GregorianCalendar(), 2, "", 0));
+        taskList.add(new Task("Sample2", new GregorianCalendar(), 2, "", 0));
+        TaskListManager.saveTaskList(main, taskList);
+        assert taskList.equals(TaskListManager.loadTaskList(main));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        TaskListManager.saveTaskList(main, storedTaskList);
     }
 }
